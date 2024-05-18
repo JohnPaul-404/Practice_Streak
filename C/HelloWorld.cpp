be6e22ca448c146c221d227963c7aad33fbf7e6b
John Paul <john.p@varuns2017outlook.onmicrosoft.com>
2024-05-18 19:53:54 +0530

    First Commit

---
+++ C/HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    //was just trying to restart my touches in C programs
+    // lets Begin with Hello World, my brain said
+    printf("Hello Worlds");
+    return 0;
+}
